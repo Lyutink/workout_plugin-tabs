50028e1ee45ff6bd8666a3b0367a35304e096f0d
Lyutink <krivenko.lyuda@gmail.com>
2021-11-30 20:07:56 +0300

    tebs-plugin.js ready

--- tabs-plugin.js
+++ tabs-plugin.js
@@ -5,12 +5,16 @@ class Tabs {
     rootSelector,
     activeControlClass = 'active',
     activePaneClass = 'active',
+    activeTab = 1,
   }) {
     this._refs = this._getRefs(rootSelector);
     this._activeControlClass = activeControlClass;
     this._activePaneClass = activePaneClass;
+    this._activeTabIdx = activeTab - 1;
 
     this._bindEvents();
+
+    this._setActiveTab();
   }
 
   _getRefs(root) {
@@ -32,35 +36,60 @@ class Tabs {
       console.log('Кликнули не в ссылку');
       return;
     }
-    
-    // const currentActiveControlItem = this._refs.controls.querySelector(
-    //   `.${this._activeControlClass}`);
-    
-    //   if (currentActiveControlItem) {
-    // currentActiveControlItem.classList.remove(this._activeControlClass);
-
-    // const paneId = this._getPaneId(currentActiveControlItem);
-    // const pane = this._getPaneById(paneId);
-    // pane.classList.remove('pane--active');
-    // }
+   
+    this._removeActiveTab();
 
     const controlItem = event.target;
     controlItem.classList.add(this._activeControlClass);
     
     const paneId = this._getPaneId(controlItem);
-    const pane = this._getPaneById(paneId);
-    pane.classList.add(this._activeControlClass);
+    this._setActivePane(paneId);
   }
 
-  
-_getPaneId(control) {
-  console.log("hi",control.getAttribute('href').slice(1));
-  return control.getAttribute('href').slice(1);
-}
+  _setActiveTab() {
+    const controlItems = this._refs.controls.querySelectorAll('a');
+    const control = controlItems[this._activeTabIdx];
 
-_getPaneById(id) {
-  return this._refs.panes.querySelector(`#${id}`);
-}
+    control.classList.add(this._activeControlClass);
+
+    const paneId = this._getPaneId(control);
+    this._setActivePane(paneId);
+
+  }
+
+  _removeActiveTab() {
+     const currentActiveControlItem = this._refs.controls.querySelector(
+      `.${this._activeControlClass}`);
+    
+    if (!currentActiveControlItem) {
+      return;
+    }
+    
+    currentActiveControlItem.classList.remove(this._activeControlClass);
+
+    const paneId = this._getPaneId(currentActiveControlItem);
+    this._removeActivePane(paneId);
+    
+  }
+
+  _setActivePane(paneId) {
+    const pane = this._getPaneById(paneId);
+    pane.classList.add(this._activePaneClass);
+  }
+
+  _removeActivePane(paneId) {
+        const pane = this._getPaneById(paneId);
+    pane.classList.remove(this._activePaneClass);
+  }
+
+  _getPaneId(control) {
+    console.log("hi",control.getAttribute('href').slice(1));
+    return control.getAttribute('href').slice(1);
+  }
+
+  _getPaneById(id) {
+    return this._refs.panes.querySelector(`#${id}`);
+  }
 
 }
 
@@ -68,6 +97,7 @@ const tabs1 = new Tabs({
   rootSelector: '#tabs-1',
   activeControlClass: 'controls__item--active',
   activePaneClass: 'pane--active',
+  activeTab: 1,
 })
 
 console.log(tabs1);
