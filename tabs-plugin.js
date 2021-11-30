52edad9eabc96ec8bd3fb14778ab1c4dc93f1eaf
Lyutink <krivenko.lyuda@gmail.com>
2021-11-30 16:02:33 +0300

    up tabs-piugin.js

--- tabs-plugin.js
+++ tabs-plugin.js
@@ -1,5 +1,76 @@
 'use strict';
 
+class Tabs {
+  constructor({
+    rootSelector,
+    activeControlClass = 'active',
+    activePaneClass = 'active',
+  }) {
+    this._refs = this._getRefs(rootSelector);
+    this._activeControlClass = activeControlClass;
+    this._activePaneClass = activePaneClass;
+
+    this._bindEvents();
+  }
+
+  _getRefs(root) {
+    const refs = {};
+
+    refs.controls = document.querySelector(`${root} [data-controls]`);
+    refs.panes = document.querySelector(`${root} [data-panes]`);
+    return refs;
+  }
+
+  _bindEvents() {
+    this._refs.controls.addEventListener('click', this._onControlsClick.bind(this));
+  }
+
+  _onControlsClick(event) {
+    event.preventDefault();
+   
+      if (event.target.nodeName !== 'A') {
+      console.log('Кликнули не в ссылку');
+      return;
+    }
+    
+    // const currentActiveControlItem = this._refs.controls.querySelector(
+    //   `.${this._activeControlClass}`);
+    
+    //   if (currentActiveControlItem) {
+    // currentActiveControlItem.classList.remove(this._activeControlClass);
+
+    // const paneId = this._getPaneId(currentActiveControlItem);
+    // const pane = this._getPaneById(paneId);
+    // pane.classList.remove('pane--active');
+    // }
+
+    const controlItem = event.target;
+    controlItem.classList.add(this._activeControlClass);
+    
+    const paneId = this._getPaneId(controlItem);
+    const pane = this._getPaneById(paneId);
+    pane.classList.add(this._activeControlClass);
+  }
+
+  
+_getPaneId(control) {
+  console.log("hi",control.getAttribute('href').slice(1));
+  return control.getAttribute('href').slice(1);
+}
+
+_getPaneById(id) {
+  return this._refs.panes.querySelector(`#${id}`);
+}
+
+}
+
+const tabs1 = new Tabs({
+  rootSelector: '#tabs-1',
+  activeControlClass: 'controls__item--active',
+  activePaneClass: 'pane--active',
+})
+
+console.log(tabs1);
 // class Tabs {
 //   constructor({
 //     rootSelector,
